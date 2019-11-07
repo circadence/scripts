28fce94aca32a2e62003f1074fe3b3583f0ca9a6
pthornton <randy.thornton@yahoo.com>
2019-11-07 15:31:15 -0600

    checkpoint

--- master-c7.sh
+++ master-c7.sh
@@ -2,3 +2,12 @@
 echo Executing >> /tmp/config.log
 echo Params: 1: $1 2: $2 3: $3 4: $4>> /tmp/config.log
 
+echo STORAGE_QUEUE_NAME = \'$1\' > config.py
+echo STORAGE_ACCOUNT_NAME = \'$2\' >> config.py
+echo STORAGE_ACCOUNT_KEY = \'$3\' >> config.py
+
+sudo yum install -y python3
+/usr/bin/virtualenv -p python3 venv
+. venv/bin/activate
+venv/bin/python -m pip install -r requirements.txt
+venv/bin/python listener.py
