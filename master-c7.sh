9f6226197a13289032a4770f82cf838ab1de294b
pthornton <randy.thornton@yahoo.com>
2019-11-13 08:46:34 -0600

    checkpoint

--- master-c7.sh
+++ master-c7.sh
@@ -2,18 +2,3 @@
 echo Executing >> /tmp/config.log
 echo Params: 1: $1 2: $2 3: $3 4: $4>> /tmp/config.log
 
-cd /tmp
-
-echo STORAGE_QUEUE_NAME = \'$1\' > config.py
-echo STORAGE_ACCOUNT_NAME = \'$2\' >> config.py
-echo STORAGE_ACCOUNT_KEY = \'$3\' >> config.py
-
-/bin/wget https://github.com/circadence/scripts/raw/master/requirements.txt
-/bin/wget https://github.com/circadence/scripts/raw/master/listener.py
-
-sudo yum install -y python3
-pip3 install virtualenv
-/usr/local/bin/virtualenv -p python3 venv
-. venv/bin/activate
-venv/bin/python -m pip install -r requirements.txt
-( venv/bin/python listener.py | tee /tmp/listener.log ) &
